12e62f200b4d76bf71576ec3456448391dad08ee
Flimey <89532952+ItsFlimey@users.noreply.github.com>
2024-01-08 09:42:34 +0100

    Add files via upload

---
+++ Introducción/Tasca/probando.js
@@ -0,0 +1,8 @@
+(function () {
+    "use strict";             /* Inicio de tu código */
+    function greetMe(tuNombre) {
+        alert("Hola " + tuNombre);
+    }
+
+    greetMe("Mundo");         /* Fin de tu código */
+})();
