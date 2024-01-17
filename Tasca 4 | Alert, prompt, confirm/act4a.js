c39cc7826c0f3386d35a323fd32a081d6a270b97
Flimey <89532952+ItsFlimey@users.noreply.github.com>
2024-01-17 11:33:41 +0100

    Delete Tasca 4 | Alert, prompt, confirm directory

--- Tasca 4 | Alert, prompt, confirm/act4a.js
+++
@@ -1,5 +0,0 @@
-let nombre = "Flimey";
-let edad = 15;
-let fecha_nac = "10/02/08";
-
-alert("Tu nombre es "+nombre+"\nTienes "+edad+" años y naciste el "+fecha_nac);
--- Tasca 4 | Alert, prompt, confirm/act4c.js
+++
@@ -1,7 +0,0 @@
-let var1 = confirm("¿Es hoy Lunes?")
-if (var1==true){
-    alert("¡Hoy es lunes! :D");
-};
-if (var1==false){
-    alert("Hoy no es lunes... :(")
-};
