d7dffddbd657826ceb466e06bc5a1280ce1b34a8
Flimey <89532952+ItsFlimey@users.noreply.github.com>
2024-01-17 11:33:20 +0100

    Add files via upload

---
+++ Tasca 4 | Alert, prompt, confirm/act4c.js
@@ -0,0 +1,7 @@
+let var1 = confirm("¿Es hoy Lunes?")
+if (var1==true){
+    alert("¡Hoy es lunes! :D");
+};
+if (var1==false){
+    alert("Hoy no es lunes... :(")
+};
---
+++ Tasca 4 | Alert, prompt, confirm/act4d.js
@@ -0,0 +1,6 @@
+let nombre = prompt("¿Cual es tu nombre de usuario?");
+if (nombre == null || nombre == "") {
+    alert("Introduzca un nombre por favor");
+} else {
+    alert("¡Hola "+nombre+"!")
+}
